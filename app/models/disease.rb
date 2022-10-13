3e817d45f394fb52d1e2f14c82baaaff9c1b1a44
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-10-13 21:01:53 +0900

    アソシエーションの設定

--- app/models/disease.rb
+++ app/models/disease.rb
@@ -1,4 +1,6 @@
 class Disease < ApplicationRecord
+  belongs_to: user
+  belongs_to: hospital_clinic
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :outcome_id
 end
--- app/models/hospital_clinic.rb
+++ app/models/hospital_clinic.rb
@@ -1,5 +1,9 @@
 class HospitalClinic < ApplicationRecord
+  belongs_to :user
+  has_many :kartes
+  has_many :diseases
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :clinical_department_id
+  
 end
