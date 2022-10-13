47c0e4ba175f31fa85e52388ce77cf231abfff7f
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-10-13 18:45:59 +0900

    karteのマイグレーションの設定

--- db/migrate/20221013092946_create_kartes.rb
+++ db/migrate/20221013092946_create_kartes.rb
@@ -1,6 +1,15 @@
 class CreateKartes < ActiveRecord::Migration[6.0]
   def change
     create_table :kartes do |t|
+      t.string :doctor    ,null: false
+      t.string :subjective,null: false
+      t.string :objective ,null: false
+      t.string :assessment,null: false
+      t.string :plan      ,null: false
+      t.string :next      ,null: false
+
+      t.references :user,           null: false,foreign_key: true
+      t.references :hospital_clinic,null: false,foreign_key: true
 
       t.timestamps
     end
