9bcfa6d7f4928be6839c46ef9a1d4afa6f5c5aa0
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-10-14 00:19:17 +0900

    karteコントローラーの設定

--- app/controllers/kartes_controller.rb
+++ app/controllers/kartes_controller.rb
@@ -1,5 +1,22 @@
 class KartesController < ApplicationController
   def index
+  end
+  def new
+    @karte_record = KarteRecord.new
+  end
+  def create
+    @karte_record = KarteRecord.new(karte_record_params)
+    if @karte_record.valid?
+      @karte_record.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def karte_record_params
+    params.require(:karte_record).permit(:institution_name,:clinical_department_id,:consultation_date,:doctor,:subjective,:objective,:assessment,:plan,:next).merge(user_id: current_user.id,hospital_clinic_id: params[:hospital_clinic_id])
   end
 end
