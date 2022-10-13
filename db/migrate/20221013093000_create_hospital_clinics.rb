b4b4930ee01b191632a90223a5f7f98ea689a6bc
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-10-13 18:54:06 +0900

    hospital_clinicのマイグレーションの設定

--- db/migrate/20221013093000_create_hospital_clinics.rb
+++ db/migrate/20221013093000_create_hospital_clinics.rb
@@ -1,6 +1,10 @@
 class CreateHospitalClinics < ActiveRecord::Migration[6.0]
   def change
     create_table :hospital_clinics do |t|
+      t.string  :institution_name       ,null: false,unique: true
+      t.integer :clinical_department_id ,null: false
+
+      t.references :user,           null: false,foreign_key: true
 
       t.timestamps
     end
