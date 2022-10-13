f7fa578a556440ec50b2158605f1d951dfb6f3b8
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-10-13 19:03:26 +0900

    diseaseのマイグレーションの設定

--- db/migrate/20221013093013_create_diseases.rb
+++ db/migrate/20221013093013_create_diseases.rb
@@ -1,7 +1,10 @@
 class CreateDiseases < ActiveRecord::Migration[6.0]
   def change
     create_table :diseases do |t|
-
+      t.string  :disease_name ,null: false,unique: true 
+      t.date    :onset_date   ,null: false
+      t.integer :outcome_id
+      t.date    :outcome_date
       t.timestamps
     end
   end
