2d14abfc1d97c712654e5e2cdbe88f9d8841b71d
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-10-14 01:42:50 +0900

    アクティブハッシュの微修正

--- app/models/clinical_department.rb
+++ app/models/clinical_department.rb
@@ -9,16 +9,16 @@ class ClinicalDepartment < ActiveHash::Base
     { id: 7, name: '皮膚科' },
     { id: 8, name: '循環器科' },
     { id: 9, name: '消化器科' },
-    { id: 10, name: '腎・泌尿器科' }
-    { id: 11, name: '呼吸器科' }
-    { id: 12, name: '糖尿病・内分泌科' }
-    { id: 13, name: '脳神経科' }
-    { id: 14, name: '運動器・整形外科' }
-    { id: 15, name: '形成外科' }
-    { id: 16, name: '心療内科' }
-    { id: 17, name: '精神科' }
-    { id: 18, name: '産婦人科' }
-    { id: 19, name: '血液内科' }
-    { id: 20, name: 'その他' }
+    { id: 10, name: '腎・泌尿器科' },
+    { id: 11, name: '呼吸器科' },
+    { id: 12, name: '糖尿病・内分泌科' },
+    { id: 13, name: '脳神経科' },
+    { id: 14, name: '運動器・整形外科' },
+    { id: 15, name: '形成外科' },
+    { id: 16, name: '心療内科' },
+    { id: 17, name: '精神科' },
+    { id: 18, name: '産婦人科' },
+    { id: 19, name: '血液内科' },
+    { id: 20, name: 'その他' },
     ]
   end
